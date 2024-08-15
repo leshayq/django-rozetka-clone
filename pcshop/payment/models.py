ad887bc66e247b9c66e074bf455e4f267d8e8709
leshayq <oleshadesigner123@gmail.com>
2024-08-15 19:15:08 +0300

    complete order

--- pcshop/payment/models.py
+++ pcshop/payment/models.py
@@ -19,7 +19,7 @@ class ShippingAddress(models.Model):
     
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
-    shipping_adress = models.ForeignKey(ShippingAddress, on_delete=models.CASCADE, blank=True, null=True)
+    shipping_address = models.ForeignKey(ShippingAddress, on_delete=models.CASCADE, blank=True, null=True)
     amount = models.DecimalField(max_digits=9, decimal_places=2)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
--- pcshop/payment/views.py
+++ pcshop/payment/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 
 from cart.cart import Cart
@@ -25,12 +26,50 @@ def shipping(request):
     return render(request, 'payment/shipping.html', {'form': form})
 
 def checkout(request):
+    if request.user.is_authenticated:
+        shipping_address = get_object_or_404(ShippingAddress, user=request.user)
+        if shipping_address:
+            return render(request, 'payment/checkout.html', {'shipping_address': shipping_address})
     return render(request, 'payment/checkout.html')
 
 def complete_order(request):
-    return render(request, 'payment/complete_order.html')
+    if request.POST.get('action') == 'payment':
+        full_name = request.POST.get('name')
+        email = request.POST.get('email')
+        address = request.POST.get('address')
+        city = request.POST.get('city')
+        zip_code = request.POST.get('zip_code')
+
+        cart = Cart(request)
+        total_price = cart.get_total_price()
+
+        shipping_address, _ = ShippingAddress.objects.get_or_create(
+            user = request.user,
+            defaults={
+                'full_name': full_name,
+                'email': email,
+                'street_address': address,
+                'city': city,
+                'zip_code': zip_code
+            }
+        )
+
+        if request.user.is_authenticated:
+            order = Order.objects.create(user=request.user, shipping_address=shipping_address, amount=total_price)
+
+            for item in cart:
+                OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['qty'], user=request.user)
+        else:
+            order = Order.objects.create(shipping_address=shipping_address, amount=total_price)
+
+            for item in cart:
+                OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['qty'])
+
+        return JsonResponse({'success': True})
 
 def payment_success(request):
+    for key in list(request.session.keys()):
+        del request.session[key]
     return render(request, 'payment/payment_success.html')
 
 def payment_fail(request):
