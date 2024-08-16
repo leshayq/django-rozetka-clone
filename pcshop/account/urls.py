a7391b6fabb64930c07d6aeaadd42bf34b31afde
leshayq <oleshadesigner123@gmail.com>
2024-08-16 19:14:49 +0300

    password reset

--- pcshop/account/urls.py
+++ pcshop/account/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.contrib.auth import views as auth_views
 from django.shortcuts import render
 from . import views
 
@@ -12,4 +13,25 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('account_delete/', views.delete_user, name='account_delete'),
     path('account_update/', views.profile_user, name='account_update'),
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='account/password/password_reset.html', 
+        email_template_name='account/password/password_reset_email.html',
+        success_url=reverse_lazy('account:password_reset_done')
+        ), 
+        name='password_reset'),
+
+    path('password_reset_done', auth_views.PasswordResetDoneView.as_view(
+        template_name='account/password/password_reset_done.html'
+    ), name='password_reset_done'),
+
+    path('password_reset_confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(
+        template_name='account/password/password_reset_confirm.html',
+        success_url=reverse_lazy('account:password_reset_complete')
+    ),
+        name='password_reset_confirm'),
+
+    path('password_reset_complete', auth_views.PasswordResetCompleteView.as_view(
+        template_name='account/password/password_reset_complete.html'
+    ), name='password_reset_complete'),
 ]
