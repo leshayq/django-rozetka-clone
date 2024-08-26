2d444ec5ca7345b0f0c12af6065d0afbc98445f9
eclipse_mint <oleshadesigner123@gmail.com>
2024-08-26 19:43:54 +0300

    welcome page

--- pcshop/pcshop/settings.py
+++ pcshop/pcshop/settings.py
@@ -47,7 +47,7 @@ ROOT_URLCONF = 'pcshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'pcshop' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
